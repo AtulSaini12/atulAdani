04a108bf0b29a15a35b394f2c8dcbfc747775f75
Atul <atulsaini9886@gmail.com>
2022-03-14 16:10:52 +0530

    Added sort function for sorting the output array with respect to count

--- ques1.js
+++ ques1.js
@@ -1,10 +1,23 @@
+//compare function to sort the array with respect to count
+
+function compareWithRespectToCount(firstEle, secondEle) {
+  return firstEle.count > secondEle.count;
+}
+
+//function to get all stake holders for different stocks
 function getStakeHolders(portfolio) {
   let stakeHolders = [];
 
+  // USED FOR LOOP HERE BUT I ALSO COULD'VE USED .MAP() FUNCTION TO TRAVERSE OVER THE ARRAY.
   for (let stakeInd = 0; stakeInd < portfolio.length; stakeInd++) {
+    //this finds if the stock I looking at right now is already present inside my stakeholders array.
     let stake = stakeHolders.find(currHolder => {
-      return currHolder.stock && currHolder.stock == portfolio[stakeInd].stock;
+      return (
+        currHolder["stock"] &&
+        currHolder["stock"] == portfolio[stakeInd]["stock"]
+      );
     });
+
     if (stake === undefined) {
       let names = [];
       names.push(portfolio[stakeInd].name);
@@ -18,5 +31,18 @@ function getStakeHolders(portfolio) {
       stake["count"] += 1;
     }
   }
+
+  stakeHolders = stakeHolders.sort(compareWithRespectToCount);
   return stakeHolders;
 }
+
+console.log(
+  getStakeHolders([
+    { name: "Mark", stock: "FB" },
+    { name: "Steve", stock: "AAPL" },
+    { name: "Tim", stock: "AAPL" },
+    { name: "Steve", stock: "MSFT" },
+    { name: "Bill", stock: "MSFT" },
+    { name: "Bill", stock: "AAPL" },
+  ])
+);
